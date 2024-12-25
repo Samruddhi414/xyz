3c54c0cd213997db17ef8bdb31dd572f504305fe
Samruddhi414 <samruddhi4309@gmail.com>
2024-12-25 19:03:40 +0530

    Add files via upload

---
+++ Bag.java
@@ -0,0 +1,17 @@
+class Bag 
+{
+	String type;
+	String size;
+	String brand;
+	String material;
+	int price;
+
+	public void showBagDetails()
+	{
+		System.out.println("type :"+type);
+		System.out.println("size :"+size);
+		System.out.println("brand :"+brand);
+		System.out.println("material :"+material);
+		System.out.println("price :"+price);
+	}
+}
